6e003ce531f11e785cbe599b6e8734e70f8c9b04
AYODEJI <ayodejialao0@gmail.com>
2023-12-10 07:56:01 +0100

    my first

---
+++ script.js
@@ -0,0 +1,21 @@
+function me() {
+    if (why.value <=10000 && why.value >=1000){
+        who.innerHTML = "Kindly withdraw your cash 💵💵"
+    }
+    
+
+
+try{
+    if(why.value == "") throw " empty"
+     if(why.value >=10000) throw " too big to dispense"
+     if(why.value <=1000) throw " too small to dispense"
+    
+}
+catch(err){
+    
+    who.innerHTML = "amount is" + err
+}
+
+ 
+    
+}
